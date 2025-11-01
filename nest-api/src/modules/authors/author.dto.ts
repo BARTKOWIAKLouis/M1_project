5b35d60aba551b974a70cf7250c214d5ddf7fc97
Louis <louis.bartkowiak@student.junia.com>
2025-11-01 18:56:09 +0100

    author services and controller
    GetAll, GetInfo, update, delete
    Get, Get(id), Post, Delete, Patch

--- nest-api/src/modules/authors/author.dto.ts
+++ nest-api/src/modules/authors/author.dto.ts
@@ -1,4 +1,4 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString, IsInt, Min, Max} from 'class-validator';
 
 export class CreateAuthorDto {
   @IsString()
@@ -6,4 +6,30 @@ export class CreateAuthorDto {
 
   @IsString()
   lastName: string;
+}
+
+export class UpdateAuthorDto{
+  @IsString()
+  @IsOptional()
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  lastName: string;
+}
+
+export class GetAuthorsDto{
+      @IsInt()
+      @Min(1)
+      @Max(100)
+      limit: number;
+      
+      @IsInt()
+      @Min(0)
+      offset: number;
+      
+      @IsString()
+      @IsOptional()
+      
+      sort?: string;
 }
--- nest-api/src/modules/authors/author.model.ts
+++ nest-api/src/modules/authors/author.model.ts
@@ -10,3 +10,16 @@ export type CreateAuthorModel = {
   firstName: string;
   lastName: string;
 };
+
+export type UpdateAuthorModel = Partial<CreateAuthorModel>;
+
+export type FilterAuthorModel = {
+  limit: number;
+  offset: number;
+  sort?: Partial<Record<keyof AuthorModel, 'ASC' | 'DESC'>>;
+}
+
+export type GetAuthorsModel = {
+  totalCount: number;
+  data: {Authors: AuthorModel, Number_of_Books: number}[];
+}
